3521cca38b5f2044a29ca41b076ef44696a44f55
Furqan Ali <furqanali2628@gmail.com>
2026-09-21 20:51:44 +0500

    Add regression tests for profile

---
+++ tests/test_knowledge_source_profile.py
@@ -0,0 +1,12 @@
+from docs.knowledge_source import KnowledgeChunk
+from docs.knowledge_source_profile import profile_source_lengths
+
+def c(i,s,t): return KnowledgeChunk(str(i),s,"title",t,i)
+
+def test_profiles_each_source():
+    result=profile_source_lengths([c(0,"a","abc"),c(1,"a","abcdef"),c(2,"b","xy")])
+    assert result[0].minimum == 3 and result[0].maximum == 6
+    assert result[0].average == 4.5 and result[1].chunks == 1
+
+def test_empty_is_empty():
+    assert profile_source_lengths([]) == ()
