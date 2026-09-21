d344098286ca6e3dd1a90ad0d7cf5ea922ee9488
Furqan Ali <furqanali2628@gmail.com>
2026-09-21 20:39:55 +0500

    Add median knowledge chunk length (#50)
    * feat: add median knowledge chunk length
    
    * test: cover median knowledge chunk length

---
+++ tests/test_knowledge_length_median.py
@@ -0,0 +1,13 @@
+from docs.knowledge_length_median import median_chunk_length
+from docs.knowledge_source import KnowledgeChunk
+
+def test_median_chunk_length_for_odd_count():
+    chunks = [KnowledgeChunk(str(i),"a.md","A","x"*n,i) for i,n in enumerate((2,5,9))]
+    assert median_chunk_length(chunks) == 5.0
+
+def test_median_chunk_length_for_even_count():
+    chunks = [KnowledgeChunk(str(i),"a.md","A","x"*n,i) for i,n in enumerate((2,5,9,11))]
+    assert median_chunk_length(chunks) == 7.0
+
+def test_median_chunk_length_handles_empty_input():
+    assert median_chunk_length([]) == 0.0
